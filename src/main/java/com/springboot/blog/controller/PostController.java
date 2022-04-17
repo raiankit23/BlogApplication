590bd34f506d218c4800cabada3f8aa10a3eaac0
Ankit Rai <raiankit23@gmail.com>
2022-04-17 21:28:18 +0530

    mvn issue fixed

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,63 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostResponse;
+import com.springboot.blog.payload.Postdto;
+import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+
+    private PostService postService;
+
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post
+    @PostMapping
+    public ResponseEntity<Postdto> createPost(@RequestBody Postdto postdto){
+        return new ResponseEntity<>(postService.createPost(postdto), HttpStatus.CREATED);
+
+    }
+
+    // get all post rest api
+    @GetMapping
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    //get post by id rest api
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getPostById(@PathVariable(name = "id") long id){
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+
+    // update post by id rest api
+    @PutMapping("/{id}")
+    public ResponseEntity<Postdto> updatePost(@RequestBody Postdto postdto, @PathVariable(name = "id") long id){
+
+        Postdto postResponse = postService.updatePost(postdto, id);
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+        postService.deletePostById(id);
+        return new ResponseEntity<>("Post Entity Deleted Sucessfully !!!", HttpStatus.OK);
+    }
+}
